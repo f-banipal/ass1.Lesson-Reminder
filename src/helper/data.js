74d3f5985c0e9ddfa253a0c9e6c1dba6c2729bc3
Shamash <f.banipal@gmail.com>
2024-03-27 17:39:56 +0100

    Ass-1 completed

---
+++ src/helper/data.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    id: 1,
+    name: "HTML",
+    hour: "10:00 am",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    hour: " 11:15 am",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
+  },
+  {
+    id: 3,
+    name: "JAVASCRIPT",
+    hour: "13:00 pm",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    id: 4,
+    name: "REACT",
+    hour: "14:15 pm",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    id: 5,
+    name: "BOOTSTRAP",
+    hour: "16:15 pm",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
+  },
+  {
+    id: 6,
+    name: "MUI",
+    hour: "16:15 pm",
+    image: "https://mui.com/static/logo.png",
+  },
+];
